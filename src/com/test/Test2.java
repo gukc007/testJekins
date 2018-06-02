e91b1f9fa5df0bbe7a3aca953a6987302420a264
gukc <gukaicheng@ifreecomm.com>
2018-06-02 18:26:03 +0800

    测试没有引入任何包的类

--- src/com/test/Test2.java
+++ src/com/test/Test2.java
@@ -7,6 +7,7 @@ public class Test2 {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
+            System.out.println("测试输出新的内容");
             System.out.println("testTouse chinese:中文");
         }
     }
